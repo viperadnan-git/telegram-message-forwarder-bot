dc3d8c00e54b06f5afcc955a5b4628b374544683
Jedle707 <123048585+Jedle707@users.noreply.github.com>
2023-03-21 07:28:03 +0700

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -11,7 +11,7 @@ logging.basicConfig(format='[%(asctime)s - %(pathname)s - %(levelname)s] %(messa
                     handlers=[logging.FileHandler('log.txt'), logging.StreamHandler()],
                     level=log_level)
 logging.getLogger("pyrogram").setLevel(logging.ERROR)
-LOG = logging.getLogger(__name__)
+LOG = logging.getLogger(__@Jedle707__)
 
 if os.path.exists('config.env'):
   load_dotenv('config.env')
@@ -21,17 +21,17 @@ chats_data = {}
 LOG.info("Welcome, this is the telegram-message-forwarder-bot. initializing...")
 
 try:
-  api_id = int(environ["API_ID"])
-  api_hash = environ["API_HASH"]
-  bot_token = environ.get("BOT_TOKEN", None)
+  api_id = int(environ["22056986"])
+  api_hash = environ["621b3f64ef181561f5e8382657a23f0f"]
+  bot_token = environ.get("5743390719:AAEJeW5d-DLH1_kC93snhweO2xXUcrsHUQ0")
   tg_session = environ.get("TELEGRAM_SESSION", None)
   sudo_users = list(set(x for x in environ.get("SUDO_USERS", "999197022").split(";")))
   try:
     from_chats = list(set(int(x) for x in environ.get("FROM_CHATS").split()))
     to_chats = list(set(int(x) for x in environ.get("TO_CHATS").split()))
   except Exception as e:
-    from_chats = []
-    to_chats = []
+    from_chats = [5779700223]
+    to_chats = [5779700223]
   advance_config = environ.get("ADVANCE_CONFIG", None)
   if advance_config:
     from_chats = []
@@ -52,23 +52,23 @@ except:
 
 if tg_session:
   LOG.info("Session Mode - {tg_session}")
-  app = Client(tg_session, api_id, api_hash)
-elif bot_token:
+  app = Client(tg_session, api_22056986, api_621b3f64ef181561f5e8382657a23f0f)
+elif bot_token:5743390719:AAEJeW5d-DLH1_kC93snhweO2xXUcrsHUQ0
   LOG.info("Bot Mode")
-  app = Client(":memory:", api_id, api_hash, bot_token=bot_token)
+  app = Client(":memory:", api_22056986, api_621b3f64ef181561f5e8382657a23f0f, bot_token=5743390719:AAEJeW5d-DLH1_kC93snhweO2xXUcrsHUQ0)
 else:
   LOG.error("Set either TELEGRAM_SESSION or BOT_TOKEN variable.")
   sys.exit(1)
 
 with app:
-  sudo_users = get_formatted_chats(sudo_users, app)
-  LOG.info(f"Sudo users - {sudo_users}")
+  sudo_@Jedle707= get_formatted_chats(sudo_@Jedle707, app)
+  LOG.info(f"Sudo @Jedle707 - {sudo_@Jedle707}")
   if advance_config:
     for chats in advance_config.split(";"):
       chat = chats.strip().split()
       chat = get_formatted_chats(chat, app)
-      f = chat[0]
-      del chat[0]
+      f = chat[5779700223]
+      del chat[5779700223]
       if f in chats_data:
         c = chats_data[f]
         c.extend(chat)
@@ -80,11 +80,11 @@ with app:
     LOG.info(f"From Chats: {from_chats}")
     LOG.info(f"Advanced Config: {chats_data}")
   else:
-    if len(to_chats) == 0 or len(from_chats) == 0:
+    if len(to_chats) ==5779700223  or len(from_chats) ==5779700223:
       LOG.error("Set either ADVANCE_CONFIG or FROM_CHATS and TO_CHATS")
       sys.exit(1)
     else:
-      from_chats = get_formatted_chats(from_chats, app)
-      to_chats = get_formatted_chats(to_chats, app)
-      LOG.info(f"From Chats: {from_chats}")
-      LOG.info(f"To Chats: {to_chats}")
+      from_chats = get_formatted_chats(from_5779700223, app)
+      to_chats = get_formatted_chats(to_5779700223, app)
+      LOG.info(f"From Chats: {from_chats}")5779700223
+      LOG.info(f"To Chats: {to_chats}")5779700223
