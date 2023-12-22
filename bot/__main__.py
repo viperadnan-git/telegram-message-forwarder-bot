12c66c61ef612e863faf90613ea7e9904233c9e3
Adnan Ahmad <viperadnan@gmail.com>
2023-12-22 19:06:43 +0530

    fixed forwarding function, fix #27

--- bot/__main__.py
+++ bot/__main__.py
@@ -1,6 +1,7 @@
 import random
 import logging
 from time import sleep
+import traceback
 
 from pyrogram import filters
 
@@ -36,8 +37,8 @@ def work(_:Client, message:Message):
 
 
 @app.on_message(filters.user(sudo_users) & filters.command(["fwd", "forward"]), group=1)
-def forward(app, message):
-    if len(message.command) > 1:
+def forward(client:Client, message:Message):
+    if len(message.command) > 1 and message.command[1].isdigit():
         chat_id = int(message.command[1])
         if chat_id:
             try:
@@ -47,23 +48,19 @@ def forward(app, message):
                     limit = int(message.command[2])
                 if len(message.command) > 3:
                     offset_id = int(message.command[3])
-                for msg in app.iter_history(chat_id, limit=limit, offset_id=offset_id):
+                for msg in client.get_chat_history(chat_id, limit=limit, offset_id=offset_id):
                     msg.copy(message.chat.id)
-                    sleep(random.randint(1, 10))
+                    sleep(random.randint(1, 5))
             except Exception as e:
-                message.reply_text(f"```{e}```")
+                message.reply_text(f"Error:\n```{traceback.format_exc()}```")
         else:
-            reply = message.reply_text(
-                "```Invalid Chat Identifier. Give me a chat id, username or message link.```"
+            message.reply_text(
+                "Invalid Chat Identifier. Give me a chat id."
             )
-            sleep(5)
-            reply.delete()
     else:
-        reply = message.reply_text(
-            "```Invalid Command ! Use /fwd {ChatID} {limit} {FirstMessageID}```"
+        message.reply_text(
+            "Invalid Command\nUse /fwd {chat_id} {limit} {first_message_id}"
         )
-        sleep(20)
-        reply.delete()
 
 
 app.run()
